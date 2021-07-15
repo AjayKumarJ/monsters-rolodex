a90907383377115cfd9ecfe09f4fadeaecf85969
ajaykumarj <jaksonline@gmail.com>
2021-07-15 17:41:16 +0530

    Updates

---
+++ precache-manifest.84d6ed61f75515a66bdc06ecf6b2c1e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f53b7fe4f1ae995afa68be5d67460b2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "139a999b8385b0228653",
+    "url": "/monsters-rolodex/static/css/main.f251e27e.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "139a999b8385b0228653",
+    "url": "/monsters-rolodex/static/js/main.1ad24c20.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
